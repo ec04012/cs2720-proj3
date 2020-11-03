73e78cc4dc27c47219e76cbb67edad8f4a99bf6b
ec04012 <ec04012@uga.edu>
2020-11-02 20:16:25 -0500

    Added python script to generate random customers

---
+++ Random.py
@@ -0,0 +1,28 @@
+import random
+
+customers = []
+
+# Generate 150 random customers
+for i in range(0, 150):	
+	customer = {
+		"name" : "Customer" + str(i + 1),
+		"arrival" : random.randint(0,25200),
+		"service" : random.randint(120, 480)
+	}
+	customers.append(customer)
+
+# Sort customers from arrive time
+customers = sorted(customers, key = lambda i: i['arrival'])
+# Rename customers
+for i in range(0, len(customers)):
+	customer = customers[i]
+	customer["name"] = "Customer" + str(i+1)
+
+# Write to file
+with open("randCustomers.txt", "w") as f:
+	for customer in customers:
+		f.write(customer["name"].ljust(13))
+		f.write("arrival " + str(customer["arrival"]).rjust(5) + "\t")
+		f.write("service " + str(customer["service"]))
+		f.write("\n")
+	
